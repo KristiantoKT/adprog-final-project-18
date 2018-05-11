e992ae65ae3c0864be3aad65463154146791cf22
ilham rizky <ilhamrizkyakbar@gmail.com>
2018-05-11 19:18:33 +0700

    Fixing some test errors, implements all tests, passed all tests

--- src/test/java/advprog/example/bot/comic/ScrappingTest.java
+++ src/test/java/advprog/example/bot/comic/ScrappingTest.java
@@ -10,17 +10,17 @@ import static org.junit.Assert.assertNotNull;
 
 public class ScrappingTest {
     Scrapping scrapping;
-    String monthlyApril = "Top 10 Oricon April Charts \n"+
-            "1. 進撃の巨人 - (画)諫山創 \n"+
-            "2. キングダム - (画)原泰久\n"+
-            "3. 僕のヒーローアカデミア - (画)堀越 耕平\n"+
-            "4. 七つの大罪 - (画)鈴木央\n"+
-            "5. よつばと! - (画)あずまきよひこ\n"+
-            "6. ゴールデンカムイ - (画)野田サトル\n"+
-            "7. ダンジョン飯 - (画)九井諒子\n"+
-            "8. ハイキュー!! - (画)古舘春一\n"+
-            "9. ワンパンマン - (画)村田 雄介\n"+
-            "10. 約束のネバーランド - (画)(出水 ぽすか\n";
+    String monthlyApril = "Top 10\n"+
+            "(1) 進撃の巨人 - (画)諫山創\n"+
+            "(2) キングダム - (画)原泰久\n"+
+            "(3) 僕のヒーローアカデミア - (画)堀越耕平\n"+
+            "(4) 七つの大罪 - (画)鈴木央\n"+
+            "(5) よつばと! - (画)あずまきよひこ\n"+
+            "(6) ゴールデンカムイ - (画)野田サトル\n"+
+            "(7) ダンジョン飯 - (画)九井諒子\n"+
+            "(8) ハイキュー!! - (画)古舘春一\n"+
+            "(9) ワンパンマン - (原作)ONE/(画)村田雄介\n"+
+            "(10) 約束のネバーランド - (原作)白井カイウ/(画)出水ぽすか";
 
     String testUrl = "https://www.oricon.co.jp/rank/cbm/m/2018-04/";
 
@@ -38,7 +38,7 @@ public class ScrappingTest {
     @Test
     public void sampleTest() throws IOException{
         scrapOut = scrapping.scrap(testUrl);
-        assertEquals(testUrl,scrapOut);
+        assertEquals(monthlyApril,scrapOut);
     }
 
 }
