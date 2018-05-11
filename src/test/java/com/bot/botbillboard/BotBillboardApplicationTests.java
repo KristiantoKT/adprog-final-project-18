1bc4a00a52741da4301adca93e8f75f8294f4cd3
nadhifah hanan <nadhifahhnn@gmail.com>
2018-05-11 21:00:38 +0700

    implementing function

---
+++ src/test/java/com/bot/botbillboard/BotBillboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bot.botbillboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotBillboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
