c931e8a2cb57a3db2464a75b8763c343788e95e0
Tyagita Larasati <tiagita@gmail.com>
2018-05-11 18:59:18 +0700

    add class to get data from web #2

---
+++ src/main/java/advprog/popularhotcountry/bot/controller/Charts.java
@@ -0,0 +1,32 @@
+package advprog.popularhotcountry.bot.controller;
+
+public class Charts {
+    private String song;
+    private String artist;
+
+    public Charts(String song, String artist) {
+        this.song = song;
+        this.artist = artist;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return artist + " - " + song;
+    }
+}
