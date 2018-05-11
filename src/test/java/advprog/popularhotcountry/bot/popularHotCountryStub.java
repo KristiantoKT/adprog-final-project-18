a3483f25141623b1006ccfe656455c9b1ae1670b
Tyagita Larasati <tiagita@gmail.com>
2018-05-11 11:15:46 +0700

    unit test + stub #2

---
+++ src/test/java/advprog/popularhotcountry/bot/popularHotCountryStub.java
@@ -0,0 +1,12 @@
+package advprog.popularhotcountry.bot;
+
+
+interface Popular {
+    String doSomething();
+}
+public class popularHotCountryStub implements Popular {
+    @Override
+    public String doSomething() {
+        return "";
+    }
+}
