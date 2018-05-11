cd0f25c3f8789491153bd81eed9833c38490c2b3
pocket <pockethp@pockets.local>
2018-05-11 21:20:57 +0700

    Added implementation for ArtistBill200 class (#12)

--- src/main/java/advprog/favArtist/bot/ArtistBill200.java
+++ src/main/java/advprog/favArtist/bot/ArtistBill200.java
@@ -1,7 +1,5 @@
 package advprog.favArtist.bot;
 
-import static java.lang.String.format;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +20,46 @@ public class ArtistBill200 {
     }
 
     public String findArtistInChart(String artistToFind, String url) {
-        return "";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.getElementsByClass("chart-row");
+
+            for (int i = 0; i < 50; i++) {
+                Element e = links.get(i);
+                String title = e.getElementsByClass("chart-row__song").html();
+                String artist = e.getElementsByClass("chart-row__artist").html();
+
+                String unescapedTitle = Parser.unescapeEntities(title, false);
+                String unescapedArtist = Parser.unescapeEntities(artist, false);
+
+                if (unescapedArtist.equalsIgnoreCase(artistToFind)) {
+                    songByArtist.add(new Song(unescapedTitle, unescapedArtist, i + 1));
+                }
+            }
+
+            return printSong();
+
+        } catch (IOException e) {
+            return "Error!";
+        }
     }
 
     public String printSong() {
-        return "";
+        if (songByArtist.size() == 0) {
+            return "Error! The artist isn't on the Billboard 200 List!";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int counter = 1;
+        for (Song song : songByArtist) {
+            stringBuilder.append(song.getArtist() + "\n" + song.getTitle() + "\n" + song.getPosition());
+            if (counter < songByArtist.size()) {
+                counter++;
+                stringBuilder.append("\n");
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
