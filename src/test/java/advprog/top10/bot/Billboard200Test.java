17a52e6737be77aa8a234f1d16da5e1581392b7d
pocket <pockethp@pockets.local>
2018-05-11 18:38:20 +0700

    Added a few tests (#3)

--- src/test/java/advprog/top10/bot/Billboard200Test.java
+++ src/test/java/advprog/top10/bot/Billboard200Test.java
@@ -1,5 +1,23 @@
 package advprog.top10.bot;
 
-public class Billboard200Test {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Billboard200Test {
+    Billboard200App billboard200;
+    String rss ="https://www.billboard.com/rss/charts/billboard-200";
+
+    @Before
+    public void setUp() {
+        billboard200 = new Billboard200App(rss);
+    }
+
+    @Test
+    public void getTop10(){
+        String output = billboard200.printTop10();
+        assertTrue(output.contains("Post Malone"));
+    }
 }
