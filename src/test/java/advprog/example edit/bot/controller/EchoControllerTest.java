c5c2912a63f0273c15af6d0c10c02969f9f2dc5b
nadhifah hanan <nadhifahhnn@gmail.com>
2018-05-11 11:22:27 +0700

    add stub func

--- src/test/java/advprog/example edit/bot/controller/EchoControllerTest.java
+++ src/test/java/advprog/example edit/bot/controller/EchoControllerTest.java
@@ -21,6 +21,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.*;
+
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
 public class EchoControllerTest {
