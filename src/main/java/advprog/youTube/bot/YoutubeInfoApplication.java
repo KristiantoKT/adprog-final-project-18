4643feca981df13d8cf20200f419ae27ed95c172
Fannyah Dita Cahya <fannyahdc@gmail.com>
2018-05-11 12:36:51 +0700

    test heroku #9

--- src/main/java/advprog/youTube/bot/YoutubeInfoApplication.java
+++ src/main/java/advprog/youTube/bot/YoutubeInfoApplication.java
@@ -4,11 +4,18 @@ import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YoutubeInfoApplication {
+public class YoutubeInfoApplication extends SpringBootServletInitializer {
 
     private static final Logger LOGGER = Logger.getLogger(YoutubeInfoApplication.class.getName());
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(YoutubeInfoApplication.class);
+    }
 
     public static void main(String[] args) {
         LOGGER.info("Application starting ...");
