fd6df5a1c92917a04e9e2d666e043e20c356d962
Daninsky <arceusgummi@gmail.com>
2018-05-11 20:48:22 +0700

    edit controller

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -1,12 +1,16 @@
 package advprog.example.bot.controller;
 
+import advprog.example.bot.oricon.oriconcommand.RankCommand;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import advprog.example.bot.oricon.oriconcommand.DailyRankCommand;
+import advprog.example.bot.oricon.oriconcommand.RankCommand;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @LineMessageHandler
@@ -14,12 +18,19 @@ public class EchoController {
 
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
 
+    private DailyRankCommand drc = new DailyRankCommand();
+
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
+        String[] splitter = contentText.split(" ");
+
+        if (splitter.length == 4 && splitter[0].equals("/oricon") && splitter[2].equals("daily")) {
+            return drc.execute(splitter[3]);
+        }
 
         String replyText = contentText.replace("/echo", "");
         return new TextMessage(replyText.substring(1));
