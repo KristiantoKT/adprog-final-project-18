682636189763afcad117c12a002a07cec2992c8d
ilham rizky <ilhamrizkyakbar@gmail.com>
2018-05-11 20:36:16 +0700

    fix some style issues

--- src/main/java/advprog/example/bot/comic/Scrapping.java
+++ src/main/java/advprog/example/bot/comic/Scrapping.java
@@ -1,13 +1,14 @@
 package advprog.example.bot.comic;
 
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 
 public class Scrapping {
@@ -19,27 +20,16 @@ public class Scrapping {
             String output = "Top 10\n";
             output += comics.stream().map(js -> scrap(js)).collect(Collectors.joining("\n"));
             return output;
-        }
-        catch (HttpStatusException e) {
+        } catch (HttpStatusException e) {
             return "Invalid url";
         }
     }
 
-    private String scrap(Element comic){
+    private String scrap(Element comic) {
         String chartPosition = comic.select("p.num").text();
         String title = comic.select("h2.title").text();
         String author = comic.select("li.artist-name").text();
         return String.format("(%s) %s - %s",chartPosition,title,author);
 
     }
-
-
-    //public static void main(String[] args) throws IOException{
-            //Scrapping py= new Scrapping();
-            //System.out.println(py.scrap("https://www.oricon.co.jp/rank/cbm/w/2018-05-14/"));
-        //}
-
-
-
-
-    }
+}
--- src/test/java/advprog/example/bot/comic/ScrappingTest.java
+++ src/test/java/advprog/example/bot/comic/ScrappingTest.java
@@ -1,32 +1,31 @@
 package advprog.example.bot.comic;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.jupiter.api.Test;
 
 public class ScrappingTest {
     Scrapping scrapping;
-    String monthlyApril = "Top 10\n"+
-            "(1) 進撃の巨人 - (画)諫山創\n"+
-            "(2) キングダム - (画)原泰久\n"+
-            "(3) 僕のヒーローアカデミア - (画)堀越耕平\n"+
-            "(4) 七つの大罪 - (画)鈴木央\n"+
-            "(5) よつばと! - (画)あずまきよひこ\n"+
-            "(6) ゴールデンカムイ - (画)野田サトル\n"+
-            "(7) ダンジョン飯 - (画)九井諒子\n"+
-            "(8) ハイキュー!! - (画)古舘春一\n"+
-            "(9) ワンパンマン - (原作)ONE/(画)村田雄介\n"+
-            "(10) 約束のネバーランド - (原作)白井カイウ/(画)出水ぽすか";
+    String monthlyApril = "Top 10\n"
+            + "(1) 進撃の巨人 - (画)諫山創\n"
+            + "(2) キングダム - (画)原泰久\n"
+            + "(3) 僕のヒーローアカデミア - (画)堀越耕平\n"
+            + "(4) 七つの大罪 - (画)鈴木央\n"
+            + "(5) よつばと! - (画)あずまきよひこ\n"
+            + "(6) ゴールデンカムイ - (画)野田サトル\n"
+            + "(7) ダンジョン飯 - (画)九井諒子\n"
+            + "(8) ハイキュー!! - (画)古舘春一\n"
+            + "(9) ワンパンマン - (原作)ONE/(画)村田雄介\n"
+            + "(10) 約束のネバーランド - (原作)白井カイウ/(画)出水ぽすか";
 
     String testUrl = "https://www.oricon.co.jp/rank/cbm/m/2018-04/";
 
     String scrapOut;
 
-    public ScrappingTest() throws IOException{
+    public ScrappingTest() throws IOException {
         scrapping = new Scrapping();
     }
 
@@ -36,7 +35,7 @@ public class ScrappingTest {
     }
 
     @Test
-    public void sampleTest() throws IOException{
+    public void sampleTest() throws IOException {
         scrapOut = scrapping.scrap(testUrl);
         assertEquals(monthlyApril,scrapOut);
     }
