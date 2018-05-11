9e59f5b5649e8384f16e57cef42f96fe8215d8ea
Kristianto <liauwkristianto@gmail.com>
2018-05-11 10:38:16 +0700

    Create stub and unit test for Song class (#21)
    Song class will be the representation of Song object that contains detail information about Song, such as its name and artist

---
+++ src/main/java/advprog/example/bot/tropical/Song.java
@@ -0,0 +1,30 @@
+package advprog.example.bot.tropical;
+
+/**
+ * 
+ */
+public class Song {
+    private String name;
+    private String artist;
+
+    public Song(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public String getArtist() {
+        return null;
+    }
+
+    public void setArtist(String artist) {
+
+    }
+}
---
+++ src/test/java/advprog/example/bot/tropical/SongTest.java
@@ -0,0 +1,37 @@
+package advprog.example.bot.tropical;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongTest {
+    Song song;
+
+    @BeforeEach
+    void setUp() {
+        song = new Song("The Final Countdown", "Europe");
+    }
+
+    @Test
+    void getName() {
+        assertEquals("The Final Countdown", song.getName());
+    }
+
+    @Test
+    void setName() {
+        song.setName("Carrie");
+        assertEquals("Carrie", song.getName());
+    }
+
+    @Test
+    void getArtist() {
+        assertEquals("Europe", song.getArtist());
+    }
+
+    @Test
+    void setArtist() {
+        song.setArtist("Various Artist");
+        assertEquals("Various Artist", song.getArtist());
+    }
+}
