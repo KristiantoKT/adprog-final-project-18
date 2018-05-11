3a79ff77929fad1cd448e8b154e1442ca2ac5a1e
Imbang Murtito <imbangmurtito@gmail.com>
2018-05-11 14:35:51 +0700

    Create initial unit test #35

---
+++ src/main/java/advprog/photonearby/PhotoNearby.java
@@ -0,0 +1,13 @@
+package advprog.photonearby;
+
+public class PhotoNearby {
+
+    public String[] getImg(String longitude, String latitude) {
+        String[] images = {"https://c1.staticflickr.com/4/3730/11061732505_99be9f6e03_b.jpg",
+                "https://i0.wp.com/farm3.staticflickr.com/2890/11062301485_f4953afac3_z.jpg",
+                "https://c1.staticflickr.com/8/7421/11098131876_d743d98a13_b.jpg",
+                "https://c1.staticflickr.com/4/3714/11061791594_e5ca170069_b.jpg",
+                "https://c1.staticflickr.com/6/5545/10903802176_6bfaa77587_b.jpg"};
+        return images;
+    }
+}
---
+++ src/test/java/advprog/photonearby/PhotoNearbyTest.java
@@ -0,0 +1,53 @@
+package advprog.photonearby;
+
+import advprog.photonearby.PhotoNearby;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class PhotoNearbyTest {
+    private static final String[] NORMAL_IMG_SET =
+                    {"https://c1.staticflickr.com/4/3730/11061732505_99be9f6e03_b.jpg",
+                    "https://i0.wp.com/farm3.staticflickr.com/2890/11062301485_f4953afac3_z.jpg",
+                    "https://c1.staticflickr.com/8/7421/11098131876_d743d98a13_b.jpg",
+                    "https://c1.staticflickr.com/4/3714/11061791594_e5ca170069_b.jpg",
+                    "https://c1.staticflickr.com/6/5545/10903802176_6bfaa77587_b.jpg"};
+    private static final String[] LESS_THAN_5_IMG_SET =
+                    {"https://c1.staticflickr.com/4/3730/11061732505_99be9f6e03_b.jpg",
+                    "https://i0.wp.com/farm3.staticflickr.com/2890/11062301485_f4953afac3_z.jpg",
+                    "https://c1.staticflickr.com/6/5545/10903802176_6bfaa77587_b.jpg"};
+    private static final String[] ERROR_MESSAGE = {"Sorry no image taken near your current location"};
+
+    private PhotoNearby searchService;
+
+    @Before
+    public void setUp() {
+        searchService = mock(PhotoNearby.class);
+    }
+
+    @Test
+    public void testGet5ImageURLThatAreTakenNearby(){
+        when(searchService.getImg("-6", "106")).thenReturn(NORMAL_IMG_SET);
+        assertEquals(NORMAL_IMG_SET, searchService.getImg("-6", "106"));
+        verify(searchService).getImg("-6", "106");
+    }
+
+    @Test
+    public void testLessThan5ImageThatareTakenNearby(){
+        when(searchService.getImg("-5", "100")).thenReturn(LESS_THAN_5_IMG_SET);
+        assertEquals(LESS_THAN_5_IMG_SET, searchService.getImg("-5", "100"));
+        verify(searchService).getImg("-5", "100");
+
+    }
+
+    @Test
+    public void testNoImageThatAreTakenNearby() {
+        when(searchService.getImg("0", "0")).thenReturn(ERROR_MESSAGE);
+        assertEquals(ERROR_MESSAGE, searchService.getImg("0", "0"));
+        verify(searchService).getImg("0", "0");
+    }
+}
