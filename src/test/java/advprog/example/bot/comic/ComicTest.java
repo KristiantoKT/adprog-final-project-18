affbdd164d4af8df82805b7b2066806134e1f392
ilham rizky <ilhamrizkyakbar@gmail.com>
2018-05-11 11:30:47 +0700

    fixin import for test

--- src/test/java/advprog/example/bot/comic/ComicTest.java
+++ src/test/java/advprog/example/bot/comic/ComicTest.java
@@ -1,5 +1,6 @@
 package advprog.example.bot.comic;
 
+import advprog.example.bot.Comic.Comic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
