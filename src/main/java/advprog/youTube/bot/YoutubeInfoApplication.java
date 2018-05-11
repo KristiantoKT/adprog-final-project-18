5f32141cfafc60289725f5edec59949c2cccc6a7
Fannyah Dita Cahya <fannyahdc@gmail.com>
2018-05-11 11:51:34 +0700

    initial test and app progress #9

---
+++ src/main/java/advprog/youTube/bot/YoutubeInfoApplication.java
@@ -0,0 +1,17 @@
+package advprog.youTube.bot;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeInfoApplication {
+
+    private static final Logger LOGGER = Logger.getLogger(YoutubeInfoApplication.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Application starting ...");
+        SpringApplication.run(YoutubeInfoApplication.class, args);
+    }
+}
