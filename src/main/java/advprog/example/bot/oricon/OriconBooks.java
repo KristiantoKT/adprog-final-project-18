a8f244d5b0411583d890c6b12459c752a0e8768f
Kristianto <liauwkristianto@gmail.com>
2018-05-13 18:53:55 +0700

    Add stub and unit test for OriconBook (#7)

---
+++ src/main/java/advprog/example/bot/oricon/OriconBooks.java
@@ -0,0 +1,19 @@
+package advprog.example.bot.oricon;
+
+import java.util.List;
+
+public class OriconBooks {
+    private List<Book> topTenBookList;
+
+    public OriconBooks(String oriconUrl, String rankingDate) {
+
+    }
+
+    private void setTopTenBookList(String url, String date) {
+
+    }
+
+    public String printTopTenList() {
+        return null;
+    }
+}
---
+++ src/test/java/advprog/example/bot/oricon/OriconBooksTest.java
@@ -0,0 +1,23 @@
+package advprog.example.bot.oricon;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OriconBooksTest {
+    private OriconBooks oriconBooks;
+
+    @BeforeEach
+    void setUp() {
+        String url = "https://www.oricon.co.jp/rank/ob/w/";
+        String date = "2018-05-14";
+        oriconBooks = new OriconBooks(url, date);
+    }
+
+    @Test
+    void printTopTenList() {
+        String expectedOutput = oriconBooks.printTopTenList();
+        assertTrue(expectedOutput.contains("漫画 君たちはどう生きるか"));
+    }
+}
