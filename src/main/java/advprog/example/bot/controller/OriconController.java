36feb5319cdfb03f5d3b9f488aa0ba6cb5d68d22
Daninsky <arceusgummi@gmail.com>
2018-05-13 16:01:30 +0700

    added oricon controller

--- src/main/java/advprog/example/bot/controller/OriconController.java
+++ src/main/java/advprog/example/bot/controller/OriconController.java
@@ -1,5 +1,6 @@
 package advprog.example.bot.controller;
 
+import advprog.example.bot.oricon.oriconcommand.WeeklyRankCommand;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -7,22 +8,30 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @LineMessageHandler
-public class EchoController {
+public class OriconController {
 
-    private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(OriconController.class.getName());
+    private WeeklyRankCommand drc = new WeeklyRankCommand();
 
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
+        String[] splitText = contentText.split(" ");
+        System.out.println(splitText.length);
+        System.out.println(splitText[2]);
 
-        String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
+        if (splitText.length == 4 && splitText[2].equals("weekly")) {
+            return drc.execute(splitText[3]);
+        }
+
+        return new TextMessage("something wrong");
     }
 
     @EventMapping
