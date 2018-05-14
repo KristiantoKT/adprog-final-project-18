91d38171c1e005531a36877f49d12524fd799893
Azka Ali <azkali20@gmail.com>
2018-05-14 21:45:14 +0700

    Create stub and some untested tests

---
+++ src/main/java/advprog/sentiment/bot/BotSentimentApplication.java
@@ -0,0 +1,17 @@
+package advprog.sentiment.bot;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class BotSentimentApplication {
+
+    private static final Logger LOGGER = Logger.getLogger(BotSentimentApplication.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Application starting ...");
+        SpringApplication.run(BotSentimentApplication.class, args);
+    }
+}
