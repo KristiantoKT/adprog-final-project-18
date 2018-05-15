a6615775cccb2ab7b94422a56eaee6c5ddac2da6
Lzadhito <lzadhito@gmail.com>
2018-05-15 14:12:36 +0700

    trying image analyzer

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -2,11 +2,14 @@ package advprog.example.bot.controller;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @LineMessageHandler
@@ -26,8 +29,25 @@ public class EchoController {
     }
 
     @EventMapping
+    public TextMessage imageAnalyzer(MessageEvent<ImageMessageContent> event) {
+        // Format and display the JSON response.
+        ImageMessageContent content = event.getMessage();
+        String jsonString = content.toString();
+        JSONObject json = new JSONObject(jsonString);
+        String reply = ("REST Response:\n");
+        reply = reply + (json.toString(2));
+        return new TextMessage(reply);
+    }
+
+    @EventMapping
     public void handleDefaultMessage(Event event) {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
     }
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
