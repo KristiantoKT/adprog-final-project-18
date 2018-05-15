a6dcec1091b2154127800d3d6a13761df7861e2b
nadhifah hanan <nadhifahhnn@gmail.com>
2018-05-16 00:20:45 +0700

    test

---
+++ src/main/java/advprog/example/bot/controller/Billboardjapan100.java
@@ -0,0 +1,69 @@
+package advprog.example.bot.controller;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
+public class Billboardjapan100 {
+    private String billboardUrl;
+    private List<Song> songByArtistInChart;
+
+    public Billboardjapan100(String billboardUrl) {
+        this.billboardUrl = billboardUrl;
+        songByArtistInChart = new ArrayList<>();
+    }
+
+    public String findArtistInChart(String artistSearch) {
+        try {
+            Document doc = Jsoup.connect(billboardUrl).get();
+            Elements links = doc.getElementsByClass("chart-row");
+
+            for (int i = 0; i < 100; i++) {
+                Element e = links.get(i);
+                String name = e.getElementsByClass("chart-row__song").html();
+                String artist = e.getElementsByClass("chart-row__artist").html();
+
+                String formatName = Parser.unescapeEntities(name, false);
+                String formatArtist = Parser.unescapeEntities(artist, false);
+
+                if (formatArtist.equalsIgnoreCase(artistSearch)) {
+                    songByArtistInChart.add(new Song(formatName, formatArtist, i + 1));
+                }
+            }
+
+            return printSong();
+
+        } catch (IOException e) {
+            return "Illegal IO";
+        }
+    }
+
+    public String printSong() {
+        if (songByArtistInChart.size() == 0) {
+            return "Error! The artist isn't on the Billboard 200 List!";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int counter = 1;
+        for (Song song : songByArtistInChart) {
+            stringBuilder.append(song.getArtist()
+                    + "\n" + song.getName() + "\n" + song.getPosition());
+            if (counter < songByArtistInChart.size()) {
+                counter++;
+                stringBuilder.append("\n");
+            }
+        }
+
+        songByArtistInChart.clear();
+
+        return stringBuilder.toString();
+    }
+}
--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -1,12 +1,17 @@
 package advprog.example.bot.controller;
 
+import advprog.example.bot.controller.Billboardjapan100;
+
+import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 @LineMessageHandler
@@ -14,15 +19,23 @@ public class EchoController {
 
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
 
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
+
+        String url = "https://www.billboard.com/charts/japan-hot-100";
+        Billboardjapan100 billboard100 = new Billboardjapan100(url);
+
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
 
-        String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
+        String replyText = contentText.replace("/billboard japan100 ", "");
+
+        return new TextMessage(billboard100.findArtistInChart(replyText));
     }
 
     @EventMapping
@@ -30,4 +43,8 @@ public class EchoController {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
     }
+
+
 }
+
+
