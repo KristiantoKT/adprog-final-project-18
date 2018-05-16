4d21a0b9fe2e5a360682739eda7c67f722c943e0
PePeW <lowell.hagabeon@ui.ac.id>
2018-05-16 22:22:10 +0700

    checkStyleTest edited v3

--- src/test/java/billboard/hot100/artist/controller/ArtistControllerTest.java
+++ src/test/java/billboard/hot100/artist/controller/ArtistControllerTest.java
@@ -15,8 +15,8 @@ import com.linecorp.bot.model.message.TextMessage;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
