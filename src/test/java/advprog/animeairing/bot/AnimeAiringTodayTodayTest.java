e7f7d426bfa99324833884904114437ffcb803e2
Tyagita Larasati <tiagita@gmail.com>
2018-05-16 23:49:27 +0700

    add unit test AnimeAiringTodayTest #50

--- src/test/java/advprog/animeairing/bot/AnimeAiringTodayTodayTest.java
+++ src/test/java/advprog/animeairing/bot/AnimeAiringTodayTodayTest.java
@@ -7,10 +7,16 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AnimeAiringTodayTodayTest {
     AnimeAiringToday animeAiringToday;
-    String url = "";
+    String url = "https://www.livechart.me/schedule/tv";
 
     @Before
     public void setUp() {
@@ -19,21 +25,24 @@ public class AnimeAiringTodayTodayTest {
 
     @Test
     public void testUrlNotFound() {
-        new AnimeAiringToday("");
+        new AnimeAiringToday("https://haduh.ac.id/");
     }
 
     @Test
     public void testGetUrlAnimeAiringToday() {
-        assertEquals("", animeAiringToday);
+        assertEquals(url, animeAiringToday.getAnimeUrlAiringToday());
     }
 
     @Test
     public void testGetAnimeAiringToday() {
-        assertTrue(true);
+        String expected = animeAiringToday.listAiringToday();
+        System.out.println(expected);
+        assertNotNull(expected);
     }
 
     @Test
     public void testGetAnimeAiringTodayList() {
-        assertEquals("", animeAiringToday);
+        ArrayList<Anime> listAiringToday = animeAiringToday.getAiringToday();
+        assertNotNull(listAiringToday);
     }
 }
