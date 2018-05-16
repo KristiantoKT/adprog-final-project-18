3ffca6a558cc2e31d24947d5c3eeb31c23d48ce0
nadhifah hanan <nadhifahhnn@gmail.com>
2018-05-16 12:18:55 +0700

    adding implementation #4

--- src/test/java/advprog/example/bot/controller/EchoControllerTest.java
+++ src/test/java/advprog/example/bot/controller/EchoControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
+
 public class EchoControllerTest {
 
     static {
@@ -41,11 +42,20 @@ public class EchoControllerTest {
     @Test
     void testHandleTextMessageEvent() {
         MessageEvent<TextMessageContent> event =
-                EventTestUtil.createDummyTextMessage("/echo Lorem Ipsum");
+                EventTestUtil.createDummyTextMessage("/billboard japan100 Artistsiapa");
 
         TextMessage reply = echoController.handleTextMessageEvent(event);
 
-        assertEquals("Lorem Ipsum", reply.getText());
+        assertEquals("Oops sorry! The artist isn't on the Billboard Japan HOT 100!", reply.getText());
+
+        event = EventTestUtil.createDummyTextMessage("/billboard japan100 BTS");
+
+        reply = echoController.handleTextMessageEvent(event);
+
+        assertEquals("Artist : " + "BTS" + "\n" + "Song : " + "Don't Leave Me" + "\n" + "Rank : " + "14"+
+                "\n" + "Artist : " + "BTS" + "\n" + "Song : " + "DNA" + "\n" + "Rank : " + "23"+
+                "\n" + "Artist : " + "BTS" + "\n" + "Song : " + "Let Go" + "\n" + "Rank : " + "91"+
+                "\n" , reply.getText());
     }
 
     @Test
