c8478d9f2c6368b033d0097d690e86789bf96070
Volyando Belvadra <volyandobelvadra@gmail.com>
2018-05-16 09:23:06 +0700

    ngubah class deprecated

--- src/main/java/advprog/example/bot/controller/EchoControllerMvcConfigurer.java
+++ src/main/java/advprog/example/bot/controller/EchoControllerMvcConfigurer.java
@@ -3,10 +3,10 @@ package advprog.example.bot.controller;
 import advprog.example.bot.BotExampleApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class EchoControllerMvcConfigurer extends WebMvcConfigurerAdapter {
+public class EchoControllerMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
