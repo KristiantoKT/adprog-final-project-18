c7c52c3cecf09627cbf16fb72e1dd275466d2c0e
Luthfi Dzaky S <dzakyluthfi28@gmail.com>
2018-05-17 20:09:20 +0700

    add import for controller

--- src/main/java/advprog/fake/news/controller/FakeNewsController.java
+++ src/main/java/advprog/fake/news/controller/FakeNewsController.java
@@ -1,4 +1,14 @@
 package advprog.fake.news.controller;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.util.logging.Logger;
+
+@LineMessageHandler
 public class FakeNewsController {
 }
