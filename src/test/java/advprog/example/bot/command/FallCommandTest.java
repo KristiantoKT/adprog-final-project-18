b9c1ec4f205383eff4674f55f7c4e42072a1e6c9
ilham rizky <ilhamrizkyakbar@gmail.com>
2018-05-17 15:04:24 +0700

    Create fall & winter test

---
+++ src/test/java/advprog/example/bot/command/FallCommandTest.java
@@ -0,0 +1,33 @@
+package advprog.example.bot.command;
+
+import com.linecorp.bot.model.message.TextMessage;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class FallCommandTest {
+    private String testDate = "fall-2018";
+    private SeasonCommand command = new FallCommand();
+    private TextMessage reply;
+
+    private String sampleTitle = "Goblin Slayer";
+    private String sampleSynopsis = "A young priestess has "
+           +  "formed her first adventuring party";
+
+
+    @Test
+    public void testInstanceOf() {
+        Assert.assertTrue(command instanceof FallCommand);
+    }
+
+    @Test
+    public void testValidOutput() throws IOException {
+        reply = command.execute(testDate);
+        reply.getText().contains(sampleTitle);
+        reply.getText().contains(sampleSynopsis);
+    }
+
+}
---
+++ src/test/java/advprog/example/bot/command/WinterCommandTest.java
@@ -0,0 +1,29 @@
+package advprog.example.bot.command;
+
+import com.linecorp.bot.model.message.TextMessage;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class WinterCommandTest {
+    private String testDate = "winter-2018";
+    private SeasonCommand command = new WinterCommand();
+    private TextMessage reply;
+
+    private String sampleTitle = "Overlord II";
+    private String sampleSynopsis = "In a world where magic is everything";
+
+
+    @Test
+    public void testInstanceOf() {
+        Assert.assertTrue(command instanceof WinterCommand);
+    }
+
+    @Test
+    public void testValidOutput() throws IOException {
+        reply = command.execute(testDate);
+        reply.getText().contains(sampleTitle);
+        reply.getText().contains(sampleSynopsis);
+    }
+}
