7f5e8c3cacb054a8278c5e47e98e6faea175976f
Luthfi Dzaky S <dzakyluthfi28@gmail.com>
2018-05-17 20:21:54 +0700

    add import for test

--- src/test/java/advprog/fake/news/controller/FakeNewsControllerTest.java
+++ src/test/java/advprog/fake/news/controller/FakeNewsControllerTest.java
@@ -1,4 +1,31 @@
 package advprog.fake.news.controller;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import advprog.fake.news.EventTestUtil;
+import advprog.fake.news.FakeNewsApplication;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
+
 public class FakeNewsControllerTest {
 }
