291cba0965960635ba0e3a7435ebda4e00f51408
Imbang Murtito <imbangmurtito@gmail.com>
2018-05-18 05:50:05 +0700

    add resource file and dummy controller #51

---
+++ src/main/java/advprog/alquran/controller/AlQuranController.java
@@ -0,0 +1,20 @@
+package advprog.alquran.controller;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+public class AlQuranController {
+
+    public static TextMessage getVerse(MessageEvent<TextMessageContent> event) {
+        return null;
+    }
+
+    public static TextMessage getVerses(MessageEvent<TextMessageContent> event) {
+        return null;
+    }
+
+    public static TextMessage getSurahInfo(MessageEvent<TextMessageContent> event) {
+        return null;
+    }
+}
