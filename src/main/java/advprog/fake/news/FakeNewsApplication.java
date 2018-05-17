49ae9efabab8ecc74f7461ea8e83d5afd90deb03
Luthfi Dzaky S <dzakyluthfi28@gmail.com>
2018-05-17 20:20:42 +0700

    add main application

--- src/main/java/advprog/fake/news/FakeNewsApplication.java
+++ src/main/java/advprog/fake/news/FakeNewsApplication.java
@@ -7,4 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FakeNewsApplication {
+
+    private static final Logger LOGGER = Logger.getLogger(FakeNewsApplication.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Application starting ...");
+        SpringApplication.run(FakeNewsApplication.class, args);
+    }
 }
