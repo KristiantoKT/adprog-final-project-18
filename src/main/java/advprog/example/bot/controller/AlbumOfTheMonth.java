1d0c17733a1994dc04e2dd45e76b2c635d373c26
Fannyah Dita Cahya <fannyahdc@gmail.com>
2018-05-18 12:25:57 +0700

    expand list #53

--- src/main/java/advprog/example/bot/controller/AlbumOfTheMonth.java
+++ src/main/java/advprog/example/bot/controller/AlbumOfTheMonth.java
@@ -24,7 +24,7 @@ public class AlbumOfTheMonth {
         try {
             Document document = Jsoup.connect(url).get();
             Elements body = document.getElementsByClass("album_infobit_medium");
-            for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < 70; i++) {
                 Element element = body.get(i);
                 String albumName = element.getElementsByClass("albumtitle").get(1).text();
                 if (albumName.contains("Original Soundtrack")
