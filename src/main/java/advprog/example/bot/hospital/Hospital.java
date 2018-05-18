8fdd1ecaed20ac4564668be0228db65867ce6a38
Kristianto <liauwkristianto@gmail.com>
2018-05-18 17:01:26 +0700

    Add stub and unit test for Hospital (#60)

---
+++ src/main/java/advprog/example/bot/hospital/Hospital.java
@@ -0,0 +1,44 @@
+package advprog.example.bot.hospital;
+
+public class Hospital {
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private String phone;
+    private String imageLink;
+    private String description;
+
+    public Hospital(String name, String address, double latitude, double longitude, String phone,
+                    String imageLink, String description) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String getAddress() {
+        return null;
+    }
+
+    public double getLatitude() {
+        return 0.0;
+    }
+
+    public double getLongitude() {
+        return 0.0;
+    }
+
+    public String getPhone() {
+        return null;
+    }
+
+    public String getImageLink() {
+        return null;
+    }
+
+    public String getDescription() {
+        return null;
+    }
+}
---
+++ src/test/java/advprog/example/bot/hospital/HospitalTest.java
@@ -0,0 +1,51 @@
+package advprog.example.bot.hospital;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HospitalTest {
+    Hospital hospital;
+
+    @BeforeEach
+    void setUp() {
+        hospital = new Hospital("Name Hospital", "Margonda", -6.0011, 108.0021,
+                "(021) 12345678", "link.com", "This is example");
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Name Hospital", hospital.getName());
+    }
+
+    @Test
+    void getAddress() {
+        assertEquals("Margonda", hospital.getAddress());
+    }
+
+    @Test
+    void getLatitude() {
+        assertEquals(-6.0011, hospital.getLatitude());
+    }
+
+    @Test
+    void getLongitude() {
+        assertEquals(108.0021, hospital.getLongitude());
+    }
+
+    @Test
+    void getPhone() {
+        assertEquals("(021) 12345678", hospital.getPhone());
+    }
+
+    @Test
+    void getImageLink() {
+        assertEquals("link.com", hospital.getImageLink());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("This is example", hospital.getDescription());
+    }
+}
