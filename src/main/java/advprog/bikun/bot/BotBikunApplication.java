e99ff2b32244d5facb2ed8e3ba7cbcae1520cbd6
indraseptama28 <indraseptama28@gmail.com>
2018-05-18 20:27:52 +0700

    add folder and test

---
+++ src/main/java/advprog/bikun/bot/BotBikunApplication.java
@@ -0,0 +1,9 @@
+package advprog.bikun.bot;
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotBikunApplication {
+}
---
+++ src/test/java/advprog/bikun/bot/EventBikunTestUtil.java
@@ -0,0 +1,18 @@
+package advprog.bikun.bot;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.UserSource;
+
+import java.time.Instant;
+public class EventBikunTestUtil {
+    private EventTestUtil() {
+        // Default private constructor
+    }
+
+    public static MessageEvent<TextMessageContent> createDummyTextMessage(String text) {
+        return new MessageEvent<>("replyToken", new UserSource("userId"),
+                new TextMessageContent("id", text),
+                Instant.parse("2018-01-01T00:00:00.000Z"));
+    }
+}
