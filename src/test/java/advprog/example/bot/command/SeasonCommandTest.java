d9d563774f79117201e1d5d3c4b550fea8254aaf
ilham rizky <ilhamrizkyakbar@gmail.com>
2018-05-20 12:47:12 +0700

    create some tests

--- src/test/java/advprog/example/bot/command/SeasonCommandTest.java
+++ src/test/java/advprog/example/bot/command/SeasonCommandTest.java
@@ -1,11 +1,15 @@
 package advprog.example.bot.command;
 
+import static org.junit.Assert.assertEquals;
+
 import com.linecorp.bot.model.message.TextMessage;
 
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
 
 
 public class SeasonCommandTest {
@@ -16,11 +20,39 @@ public class SeasonCommandTest {
     private String sampleTitle = "Boku no Hero Academia 3rd Season";
     private String sampleSynopsis = "Izuku";
 
+    @Before
+    public void setUp() {
+        command.setYear("2018");
+        command.setSeason("spring");
+        command.setGenre("Action");
+        command.setUrl();
+    }
 
     @Test
     public void testInstanceOf() {
         Assert.assertTrue(command instanceof SeasonCommand);
     }
 
+    @Test
+    public void testValidUrl() {
+        assertEquals(command.getUrl(), testDate);
+    }
 
+    @Test
+    public void testGetYear() {
+
+        assertEquals("2018", command.getYear());
+    }
+
+    @Test
+    public void testGetSeason() {
+
+        assertEquals("spring", command.getSeason());
+    }
+
+    @Test
+    public void testGetGenre() {
+
+        assertEquals("Action", command.getGenre());
+    }
 }
