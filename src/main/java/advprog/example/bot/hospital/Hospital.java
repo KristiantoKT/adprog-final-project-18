123c78216b0a08f70dd90d2616e71700dc85fdd5
Kristianto <liauwkristianto@gmail.com>
2018-05-22 01:46:21 +0700

    Refactor Hospital class using Project Lombok library

--- src/main/java/advprog/example/bot/hospital/Hospital.java
+++ src/main/java/advprog/example/bot/hospital/Hospital.java
@@ -1,50 +1,22 @@
 package advprog.example.bot.hospital;
 
-public class Hospital {
-    private String name;
-    private String address;
-    private double latitude;
-    private double longitude;
-    private String phone;
-    private String imageLink;
-    private String description;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 
-    public Hospital(String name, String address, double latitude, double longitude,
-                    String phone, String imageLink, String description) {
-        this.name = name;
-        this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.phone = phone;
-        this.imageLink = imageLink;
-        this.description = description;
-    }
+public class Hospital implements Comparable<Hospital> {
+    @Getter private String name;
+    @Getter private String address;
+    @Getter private double latitude;
+    @Getter private double longitude;
+    @Getter private String phone;
+    @Getter private String imageLink;
+    @Getter private String description;
+    @Getter @Setter private int distance = -1;
 
-    public String getName() {
-        return name;
-    }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getImageLink() {
-        return imageLink;
-    }
-
-    public String getDescription() {
-        return description;
+    @Override
+    public int compareTo(@NotNull Hospital hospital) {
+        return this.distance - hospital.distance;
     }
 }
