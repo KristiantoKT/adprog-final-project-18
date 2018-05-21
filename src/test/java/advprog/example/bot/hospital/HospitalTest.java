323acb11408468c6e2539c68860eee561f60da18
Kristianto <liauwkristianto@gmail.com>
2018-05-22 01:44:35 +0700

    Edit unit test for Hospital, clean code

--- src/test/java/advprog/example/bot/hospital/HospitalTest.java
+++ src/test/java/advprog/example/bot/hospital/HospitalTest.java
@@ -4,20 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class HospitalTest {
     Hospital[] hospitals;
 
     @BeforeEach
     void setUp() throws Exception {
-//        hospital = new Hospital("Name Hospital", "Margonda", -6.0011, 108.0021,
-//                "(021) 12345678", "link.com", "This is example");
         String path = "./src/main/java/advprog/example/bot/hospital/hospital-list.json";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         ObjectMapper objectMapper = new ObjectMapper();
@@ -36,39 +32,4 @@ class HospitalTest {
         assertTrue(hospital.getImageLink().contains("tarakan"));
         assertTrue(hospital.getDescription().contains("Tarakan"));
     }
-//
-//    @Test
-//    void getName() {
-//        assertEquals("Name Hospital", hospital.getName());
-//    }
-//
-//    @Test
-//    void getAddress() {
-//        assertEquals("Margonda", hospital.getAddress());
-//    }
-//
-//    @Test
-//    void getLatitude() {
-//        assertEquals(-6.0011, hospital.getLatitude());
-//    }
-//
-//    @Test
-//    void getLongitude() {
-//        assertEquals(108.0021, hospital.getLongitude());
-//    }
-//
-//    @Test
-//    void getPhone() {
-//        assertEquals("(021) 12345678", hospital.getPhone());
-//    }
-//
-//    @Test
-//    void getImageLink() {
-//        assertEquals("link.com", hospital.getImageLink());
-//    }
-//
-//    @Test
-//    void getDescription() {
-//        assertEquals("This is example", hospital.getDescription());
-//    }
 }
