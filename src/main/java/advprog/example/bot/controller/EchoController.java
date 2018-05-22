e4de9981d88e698cc0c1c40706648efbeac0f920
Daninsky <arceusgummi@gmail.com>
2018-05-22 23:31:17 +0700

    this is starting to kill me

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -19,7 +19,6 @@ import com.linecorp.bot.model.message.template.CarouselTemplate;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
@@ -61,45 +60,50 @@ public class EchoController {
             return new AudioMessage(url,30000);
 
         } else if (inputan[0].equals("/carousel")) {
-
             String imageUrl = "https://i.schoolido.lu/songs/soldier_game.jpg";
+            CarouselColumn[] columns = new CarouselColumn[3];
+            columns[0] = new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
+                    new URIAction("Go to line.me",
+                            "https://line.me"),
+                    new URIAction("Go to line.me",
+                            "https://line.me"),
+                    new PostbackAction("Say hello1",
+                            "hello こんにちは")
+            ));
+            columns[1] = new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
+                    new PostbackAction("言 hello2",
+                            "hello こんにちは",
+                            "hello こんにちは"),
+                    new PostbackAction("言 hello2",
+                            "hello こんにちは",
+                            "hello こんにちは"),
+                    new MessageAction("Listen!!",
+                            "/listen_song Soldier Game")
+            ));
+            columns[2] = new CarouselColumn(imageUrl, "Datetime Picker",
+                    "Please select a date, time or datetime", Arrays.asList(
+                    new DatetimePickerAction("Datetime",
+                            "action=sel",
+                            "datetime",
+                            "2017-06-18T06:15",
+                            "2100-12-31T23:59",
+                            "1900-01-01T00:00"),
+                    new DatetimePickerAction("Date",
+                            "action=sel&only=date",
+                            "date",
+                            "2017-06-18",
+                            "2100-12-31",
+                            "1900-01-01"),
+                    new DatetimePickerAction("Time",
+                            "action=sel&only=time",
+                            "time",
+                            "06:15",
+                            "23:59",
+                            "00:00")
+            ));
             CarouselTemplate carouselTemplate = new CarouselTemplate(
                     Arrays.asList(
-                            new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
-                                    new URIAction("Go to line.me",
-                                            "https://line.me")
-                            )),
-                            new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
-                                    new PostbackAction("言 hello2",
-                                            "hello こんにちは",
-                                            "hello こんにちは"),
-                                    new PostbackAction("言 hello2",
-                                            "hello こんにちは",
-                                            "hello こんにちは"),
-                                    new MessageAction("Listen!!",
-                                            "/listen_song Soldier Game")
-                            )),
-                            new CarouselColumn(imageUrl, "Datetime Picker",
-                                    "Please select a date, time or datetime", Arrays.asList(
-                                    new DatetimePickerAction("Datetime",
-                                            "action=sel",
-                                            "datetime",
-                                            "2017-06-18T06:15",
-                                            "2100-12-31T23:59",
-                                            "1900-01-01T00:00"),
-                                    new DatetimePickerAction("Date",
-                                            "action=sel&only=date",
-                                            "date",
-                                            "2017-06-18",
-                                            "2100-12-31",
-                                            "1900-01-01"),
-                                    new DatetimePickerAction("Time",
-                                            "action=sel&only=time",
-                                            "time",
-                                            "06:15",
-                                            "23:59",
-                                            "00:00")
-                            ))
+                            columns
                     ));
             TemplateMessage templateMessage = new TemplateMessage(
                     "Carousel alt text", carouselTemplate);
