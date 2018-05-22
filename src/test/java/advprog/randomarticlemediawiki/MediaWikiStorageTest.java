0e122c1e8825ae7aa8458353c74e7e2edd016861
unknown <aditya211197@gmail.com>
2018-05-23 05:13:22 +0700

    Menambah test untuk Class MediaWikiStorage

--- src/test/java/advprog/randomarticlemediawiki/MediaWikiStorageTest.java
+++ src/test/java/advprog/randomarticlemediawiki/MediaWikiStorageTest.java
@@ -1,4 +1,26 @@
 package advprog.randomarticlemediawiki;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MediaWikiStorageTest {
+    MediaWikiStorage store = new MediaWikiStorage();
+
+    @Test
+    public void getUrlTest() {
+        store.addUrl("hallo");
+        String[] hasil = store.getUrl();
+        assertTrue(hasil[hasil.length-1].equals("hallo"));
+    }
+
+    @Test
+    public void getRandomArticleTest() {
+        Article hasil = store.getRandomArticle("http://marvel.wikia.com/api.php");
+        assertTrue(!hasil.getTitle().isEmpty());
+        assertTrue(!hasil.getLink().isEmpty());
+    }
 }
