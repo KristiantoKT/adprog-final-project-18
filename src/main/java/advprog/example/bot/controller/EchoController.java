c6134671b7151c3703806072cffa86cc6340c987
unknown <aditya211197@gmail.com>
2018-05-23 06:01:11 +0700

    Memasukkan command untuk LineBot (#44)

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -13,21 +13,33 @@ import java.util.logging.Logger;
 public class EchoController {
 
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
-
+    private boolean stateStatus = false;
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
-
-        String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
+        String[] arrContentText = contentText.split(" ");
+        if (arrContentText[0].equalsIgnoreCase("/echo")) {
+            String replyText = contentText.replace("/echo", "");
+            return new TextMessage(replyText.substring(1));
+        } else if (arrContentText[0].equalsIgnoreCase("/add_wiki")) {
+            return new TextMessage("");
+        } else if (arrContentText[0].equalsIgnoreCase("/random_wiki_article")) {
+            return new TextMessage("");
+        } else {
+            return new TextMessage("");
+        }
     }
 
     @EventMapping
     public void handleDefaultMessage(Event event) {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
+    }
+
+    public boolean checkValidEndPoint(String url) {
+        return false;
     }
 }
