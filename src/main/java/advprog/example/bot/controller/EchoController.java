21f48d77ed78e0be4ebdc84023c6dcd49469b89f
Daninsky <arceusgummi@gmail.com>
2018-05-22 20:38:57 +0700

    trying out carousel 2

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -3,6 +3,8 @@ package advprog.example.bot.controller;
 import advprog.anison.bot.ItuneSearch;
 import advprog.anison.bot.SongSearch;
 
+import com.linecorp.bot.model.action.DatetimePickerAction;
+import com.linecorp.bot.model.action.MessageAction;
 import com.linecorp.bot.model.action.PostbackAction;
 import com.linecorp.bot.model.action.URIAction;
 import com.linecorp.bot.model.event.Event;
@@ -58,14 +60,51 @@ public class EchoController {
             return new AudioMessage(url,30000);
 
         } else if (inputan[0].equals("/carousel")) {
+            String imageUrl = "https://vignette.wikia.nocookie.net/yuripedia/images/a/a3/1397768504009.png/revision/latest?cb=20140729170724";
             CarouselTemplate carouselTemplate = new CarouselTemplate(
                     Arrays.asList(
-                            new CarouselColumn("","hoge",
-                                    "fuga",Arrays.asList(new PostbackAction(
-                                            "Soldier Game","/listen_song Soldier Game")))
-                    )
-            );
-            TemplateMessage templateMessage = new TemplateMessage("altText",carouselTemplate);
+                            new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
+                                    new URIAction("Go to line.me",
+                                            "https://line.me"),
+                                    new URIAction("Go to line.me",
+                                            "https://line.me"),
+                                    new PostbackAction("Say hello1",
+                                            "hello こんにちは")
+                            )),
+                            new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
+                                    new PostbackAction("言 hello2",
+                                            "hello こんにちは",
+                                            "hello こんにちは"),
+                                    new PostbackAction("言 hello2",
+                                            "hello こんにちは",
+                                            "hello こんにちは"),
+                                    new MessageAction("Say message",
+                                            "Rice=米")
+                            )),
+                            new CarouselColumn(imageUrl, "Datetime Picker",
+                                    "Please select a date, time or datetime", Arrays.asList(
+                                    new DatetimePickerAction("Datetime",
+                                            "action=sel",
+                                            "datetime",
+                                            "2017-06-18T06:15",
+                                            "2100-12-31T23:59",
+                                            "1900-01-01T00:00"),
+                                    new DatetimePickerAction("Date",
+                                            "action=sel&only=date",
+                                            "date",
+                                            "2017-06-18",
+                                            "2100-12-31",
+                                            "1900-01-01"),
+                                    new DatetimePickerAction("Time",
+                                            "action=sel&only=time",
+                                            "time",
+                                            "06:15",
+                                            "23:59",
+                                            "00:00")
+                            ))
+                    ));
+            TemplateMessage templateMessage = new TemplateMessage(
+                    "Carousel alt text", carouselTemplate);
             return templateMessage;
         }
 
